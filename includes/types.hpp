c317e8b39a956466008f2590e9ef95858f06c2fd
pieric-ux <demont.pieric@gmail.com>
2025-10-17 12:27:09 +0200

    feat: types

---
+++ includes/types.hpp
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   types.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdemont <pdemont@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*   By: blucken <blucken@student.42lausanne.ch>  +#+#+#+#+#+   +#+           */
+/*                                                     #+#    #+#             */
+/*   Created: 2025/10/17                              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+#include "common.hpp"
+#include "logLevel.hpp"
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace common::core;
+
+namespace log42
+{
+
+namespace logRecord { class LogRecord; }
+namespace handler { class Handler; }
+namespace filter { class Filter; }
+namespace logger { class Logger; }
+class Node;
+
+typedef std::vector<logRecord::LogRecord> t_records;
+// typedef std::vector<raii::UniquePtr<handler::Handler> > t_handlers;
+typedef std::set<handler::Handler *> t_handlers;
+typedef std::vector<std::string> t_args;
+typedef std::map<std::string, std::string> t_defaults;
+typedef std::map<std::string, Node *> t_loggerMap;
+typedef std::set<filter::Filter> t_filters;
+typedef std::set<logger::Logger *> t_loggers;
+typedef std::map<logRecord::e_LogLevel, bool> t_cache;
+typedef std::set<Node *> t_nodes;
+
+} // !log42
+
+#endif // !TYPES_HPP
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                MIT License                                 */
+/*                                                                            */
+/*   Copyright (c) 2025 Demont Pieric, Lucken Bénédict                        */
+/*                                                                            */
+/*   Inspired by the Python 'logging' module by Vinay Sajip.                  */
+/*   This implementation was rewritten in C++98 and contains no original      */
+/*   Python source code.                                                      */
+/*                                                                            */
+/*   Permission is hereby granted, free of charge, to any person obtaining    */
+/*   a copy of this software and associated documentation files (the          */
+/*   "Software"), to deal in the Software without restriction, including      */
+/*   without limitation the rights to use, copy, modify, merge, publish,      */
+/*   distribute, sublicense, and/or sell copies of the Software, and to       */
+/*   permit persons to whom the Software is furnished to do so, subject to    */
+/*   the following conditions:                                                */
+/*                                                                            */
+/*   The above copyright notice and this permission notice shall be included  */
+/*   in all copies or substantial portions of the Software.                   */
+/*                                                                            */
+/*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS  */
+/*   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF               */
+/*   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.   */
+/*   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY     */
+/*   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,     */
+/*   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE        */
+/*   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                   */
+/*                                                                            */
+/* ************************************************************************** */
